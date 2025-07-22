1bb654aaa2331e9c28981746bb7092fc8e8128cf
Alidabonsey <bilepalida@gmail.com>
2025-07-22 17:29:34 +0100

    Remettre les couleurs d'avant

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,7 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      colors: {
-        // Charte graphique Nzoo Immo
-        'nzoo': {
-          'primary': '#183154',    // Couleur dominante du logo
-          'secondary': '#4B4F54',  // Gris Urbain
-          'light': '#D3D6DB',      // Gris Clair neutre
-        }
-      }
-    },
+    extend: {},
   },
   plugins: [],
 };
