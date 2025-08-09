e032fb834c289468c02cfc2239cf8f242f168210
Discovery-supp <scongodiscovery18@gmail.com>
2025-08-09 16:46:36 +0100

    Corriger les problèmes de déploiement Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
+    chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
         manualChunks: {
